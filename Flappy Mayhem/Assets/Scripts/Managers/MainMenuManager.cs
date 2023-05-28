b5898804bbd1e5a8ad334a05080e71447cd7c4f8
Paweł Spionkowski <pawel.spionkowski@gmail.com>
2023-05-28 13:59:53 +0200

    Validation has been added in Version 1.0.1

--- Flappy Mayhem/Assets/Scripts/Managers/MainMenuManager.cs
+++ Flappy Mayhem/Assets/Scripts/Managers/MainMenuManager.cs
@@ -1,7 +1,13 @@
+using Org.BouncyCastle.Utilities.Encoders;
+using System.Security.Cryptography;
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.TextCore.Text;
+using Nethereum.Util;
+using System.Text.RegularExpressions;
 
 public class MainMenuManager : MonoBehaviour
 {
@@ -18,7 +24,50 @@ public class MainMenuManager : MonoBehaviour
 
     public void StartGame()
     {
-        PlayerPrefs.SetString("Wallet", inputWalletText.text);
-        SceneManager.LoadScene(1);
+        string targetAddress = Regex.Replace(inputWalletText.text, @"[^\x20-\x7F]", "");
+
+        if (ValidateWallet(targetAddress))
+        {
+            inputWalletText.color = Color.green;
+
+            PlayerPrefs.SetString("Wallet", targetAddress);
+
+            SceneManager.LoadScene(1);
+        }
+        else
+        {
+            inputWalletText.color = Color.red;
+        }
+    }
+
+    public void SetColor()
+    {
+        string targetAddress = Regex.Replace(inputWalletText.text, @"[^\x20-\x7F]", "");
+
+        if (ValidateWallet(targetAddress))
+        {
+            inputWalletText.color = Color.green;
+        }
+        else
+        {
+            inputWalletText.color = Color.red;
+        }
+    }
+
+    private bool ValidateWallet(string wallet)
+    {
+        Regex r = new("^(0x){1}[0-9a-fA-F]{40}$");
+        if (!r.IsMatch(wallet))
+        {
+            return false;
+        }
+        else if (wallet == wallet.ToLower())
+        {
+            return true;
+        }
+        else
+        {
+            return new AddressUtil().IsChecksumAddress(wallet);
+        }
     }
 }
